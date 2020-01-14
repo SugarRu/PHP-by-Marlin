1e9badfd09e0eb483b59bc7c6e43cb9ca05e3312
Andy_Frost <andreyritter@rambler.ru>
2020-01-14 12:52:15 +1100

    Knowledge base BLOCK 2 - LESSON 2

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 /* -----------------------------   БЛОК 2 - УРОК 1   --------------------------------------*/
 echo "<strong> -----------------------------   БЛОК 2 - УРОК 1   ------------------------------------- </strong><br/><br/>";
 
+/* Приемы работы с флагами */
+echo "<strong> Приемы работы с флагами /strong><br/>";
+echo "<br/>";
+
+
 /* Задача 1 */
 echo "<strong> Задача 1</strong><br/>";
 echo "<br/>";
@@ -57,3 +62,44 @@ function identical($arr) {
         return "no";
 }
 echo identical($arr);
+echo "<br/>";
+echo "<br/>";
+
+
+/* -----------------------------   БЛОК 2 - УРОК 2   --------------------------------------*/
+echo "<strong> -----------------------------   БЛОК 2 - УРОК 2   ------------------------------------- </strong><br/><br/>";
+
+/* Приемы работы с логическими значениями */
+echo "<strong> Приемы работы с логическими значениями <br/>";
+echo "<br/>";
+
+/* Задача 1 */
+echo "<strong> Задача 1</strong><br/>";
+echo "<br/>";
+
+function compare($a, $b) {
+        return $a == $b;
+}
+var_dump(compare(2, 2));
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 2 */
+echo "<strong> Задача 2</strong><br/>";
+echo "<br/>";
+
+function sumCompare($a, $b) {
+        return $a + $b > 10;
+}
+var_dump(compare(5, 5)); // почему true, когда 10 !> 10, а 10 = 10? 
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 3 */
+echo "<strong> Задача 3</strong><br/>";
+echo "<br/>";
+
+function negative($a){
+        return $a < 0;
+}
+var_dump(negative(-1));

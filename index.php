7da51ffbafa542c2279b18561cbac6908ae884d6
Andy_Frost <andreyritter@rambler.ru>
2020-01-14 18:42:26 +1100

    Knowledge base BLOCK 2 - LESSON 3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 echo "<strong> -----------------------------   БЛОК 2 - УРОК 1   ------------------------------------- </strong><br/><br/>";
 
 /* Приемы работы с флагами */
-echo "<strong> Приемы работы с флагами /strong><br/>";
+echo "<strong> Приемы работы с флагами </strong><br/>";
 echo "<br/>";
 
 
@@ -70,7 +70,7 @@ echo "<br/>";
 echo "<strong> -----------------------------   БЛОК 2 - УРОК 2   ------------------------------------- </strong><br/><br/>";
 
 /* Приемы работы с логическими значениями */
-echo "<strong> Приемы работы с логическими значениями <br/>";
+echo "<strong> Приемы работы с логическими значениями </strong> <br/>";
 echo "<br/>";
 
 /* Задача 1 */
@@ -103,3 +103,89 @@ function negative($a){
         return $a < 0;
 }
 var_dump(negative(-1));
+echo "<br/>";
+echo "<br/>";
+
+
+/* -----------------------------   БЛОК 2 - УРОК 3   --------------------------------------*/
+echo "<strong> -----------------------------   БЛОК 2 - УРОК 3   ------------------------------------- </strong><br/><br/>";
+
+/* Приемы работы с циклами */
+echo "<strong> Приемы работы с циклами </strong> <br/>";
+echo "<br/>";
+
+/* Задача 1 */
+echo "<strong> Задача 1</strong><br/>";
+echo "<br/>";
+
+$str;
+for ($i = 1; $i < 10; $i++) { 
+        $str = $str . $i;
+}
+echo $str;
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 2 */
+echo "<strong> Задача 2 </strong><br/>";
+echo "<br/>";
+
+$str2;
+for ($i = 9; $i > 0; $i--) { 
+        $str2 = $str2 . $i;
+}
+echo $str2;
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 3 */
+echo "<strong> Задача 3 </strong><br/>";
+echo "<br/>";
+
+$str3;
+for ($i = 1; $i < 10; $i++) { 
+        $str3 = $str3 . '-' . $i;
+}
+echo $str3 . '-';
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 4 */
+echo "<strong> Задача 4 </strong><br/>";
+echo "<br/>";
+
+for ($i=1; $i < 21; $i++) { 
+        for ($j=0; $j < $i; $j++) { 
+               echo 'x';               
+        }
+        echo '<br>';
+}
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 5 */
+echo "<strong> Задача 5 </strong><br/>";
+echo "<br/>";
+
+for ($i=1; $i < 10; $i++) { 
+        for ($j=0; $j < $i; $j++) { 
+               echo $i;               
+        }
+        echo '<br>';
+}
+echo "<br/>";
+echo "<br/>";
+
+/* Задача 6 */
+echo "<strong> Задача 6 </strong><br/>";
+echo "<br/>";
+
+for ($i=2; $i < 12; $i+=2) { 
+        for ($j=0; $j < $i; $j++) { 
+               echo 'x';               
+        }
+        echo '<br>';
+}
+echo "<br/>";
+echo "<br/>";
+
